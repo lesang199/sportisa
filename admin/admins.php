9a7abb1864b343c044c279bf2d8fdfa4c1dfe6f0
ThinhSuycode <thinhcound96@gmail.com>
2025-05-14 16:03:24 +0700

    Admin

--- admin/admins.php
+++ admin/admins.php
@@ -77,15 +77,18 @@ try {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
-
+    <style>
+        .row{
+            height: 100vh;
+        }
+    </style>
 
 </head>
 <body>
     <div class="container-fluid">
         <div class="row">
             <!-- Sidebar -->
-           <?php include 'sidebar.php'; ?>
-
+            <?php include 'sidebar.php'; ?>
             <!-- Main content -->
             <div class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
                 <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
--- admin/orders.php
+++ admin/orders.php
@@ -45,7 +45,11 @@ $orders = $stmt->fetchAll(PDO::FETCH_ASSOC);
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
     <link href="../css/style.css" rel="stylesheet">
-
+   <style>
+    .row {
+  height: 100vh;
+}
+   </style>
 </head>
 <body>
     <div class="container-fluid">
